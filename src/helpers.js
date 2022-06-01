6622340bde728add4cb4b7a92342a66dd1e0c995
Marioegarcia <marioegarcia92@outlook.com>
2022-05-31 19:40:46 -0500

    first commit

---
+++ src/helpers.js
@@ -0,0 +1,46 @@
+export function calcularTotal(cantidad, plazo,interes,deposito){
+
+    console.log({cantidad,deposito});
+    let total = cantidad - deposito;
+   
+    let mensualidad = total / plazo;
+    
+    return total;
+    //console.log(totalPlazo);
+}
+// export function calcularTotal(cantidad, plazo,interes,deposito){
+
+//     const cantidadInt = parseInt(cantidad);
+//     const plazoInt = parseInt(plazo);
+
+//     let totalCantidad;
+//     if (cantidadInt <= 1000) {
+//         totalCantidad = cantidadInt * 0.25;
+//     } else if (cantidadInt > 1000 && cantidadInt <= 5000) {
+//         totalCantidad = cantidadInt * 0.20;
+//     }else if (cantidadInt > 5000 && cantidadInt <= 10000) {
+//         totalCantidad = cantidadInt * 0.15;
+//     }else {
+//         totalCantidad = cantidadInt * 0.10;
+//     }
+
+//     let totalPlazo;
+//     switch (plazoInt) {
+//         case 3:
+//             totalPlazo = cantidadInt * 0.05;
+//             break;
+//         case 6:
+//             totalPlazo = cantidadInt * 0.10;
+//             break;
+//         case 12:
+//             totalPlazo = cantidadInt * 0.15;
+//             break;
+//         case 24:
+//             totalPlazo = cantidadInt * 0.20;
+//             break;
+//         default:
+//             break;
+//     }
+//     return totalCantidad + totalPlazo + cantidadInt;
+//     //console.log(totalPlazo);
+// }
